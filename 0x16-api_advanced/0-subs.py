e554a07af7cb757f4038a4de94f5f1b86be0f0e0
deborahambelo <ketema.deborah@gmail.com>
2024-06-09 11:59:22 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     Queries the Reddit API and returns the number of subscribers.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'Sure-Ad-7713': 'myApp/0.0.1'}
+    headers = {'User-Agent': 'Sure-Ad-7713'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
