8b8e3fb49e60a3d344d695125cbde7ac1ffb349a
deborahambelo <ketema.deborah@gmail.com>
2024-06-09 02:34:03 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """
-    Uses Reddit API to print the number of subscribers of a subreddit
+Uses Reddit API to print the number of subscribers of a subreddit.
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Get the number of subscribers for a given subreddit
+    Get the number of subscribers for a given subreddit.
     """
+    # Keep the line length within 79 characters
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'request'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'myApp/0.0.1'}
 
-    if response.status_code != 200:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get('data', {})
+            return data.get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-
-    data = response.json().get("data")
-    subs = data.get("subscribers")
-
-    return subs
-
