a78d35f4a3d1fa1d118a0fed8a65bffe38dfcd6c
deborahambelo <ketema.deborah@gmail.com>
2024-06-09 01:52:35 -0700

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If an invalid subreddit is given, the function returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'myApp/0.0.1'}  # Include a custom User-Agent
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
